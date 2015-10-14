2ecece62b7b2bcaf34fd7fd8510dd470c140e599
Eric Shussan <ericshussan@hotmail.com>
2015-10-14 15:06:51 -0300

    IndicePluviometrico
    Verifica o Indice Pluviometrico dentro do periodo de 31 dias, exibindo o dia de pico e a media no periodo

---
+++ IndicePluviometrico.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class IndicePluviometrico {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int dias = 31, atual = 0;
+		double max = 0, media = 0, pico = 0;
+		double indicePluviometrico[] = new double[dias];
+
+		for (int i = 0; i < indicePluviometrico.length; i++) {
+			System.out.println("Digite o valor do indice pluviométrico do dia " + (atual + 1));
+			indicePluviometrico[atual] = sc.nextDouble();
+			if(indicePluviometrico[atual] > max){
+				max = indicePluviometrico[atual];
+				pico = atual+1;
+			}
+			atual++;
+		}
+
+		for (int i = 0; i < indicePluviometrico.length; i++) {
+			media += indicePluviometrico[i];
+		}
+		media = media / dias;
+
+		System.out.println("O indice pluviometrico medio eh: " + media + "\n O pico foi no dia: " + pico);
+
+	}
+
+}
