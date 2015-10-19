42171af16c1cf52563c8be3ff7bcbca71e4c1367
Eric Shussan <ericshussan@hotmail.com>
2015-10-19 15:32:27 -0200

    Tabuada
    Imprime a tabuada do 1 até o 10.

---
+++ Tabuada.java
@@ -0,0 +1,16 @@
+
+public class Tabuada {
+
+	public static void main(String[] args) {
+		System.out.println("Tabuada: \n");
+		
+		for (int i = 1; i <= 10; i++) {
+			for (int j = 1; j <= 10; j++) {
+				System.out.println(i + " X " + j + " = " + i * j);
+			}
+			System.out.println(" ");
+		}
+
+	}
+
+}
